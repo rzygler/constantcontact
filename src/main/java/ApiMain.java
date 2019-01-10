fecc6242f524ef1455cdc7df9561b5f5905f0c75
Rich Zygler <rzygler@gmail.com>
2019-01-10 18:34:05 -0500

    adding contact lists example

--- src/main/java/ApiMain.java
+++ src/main/java/ApiMain.java
@@ -1,10 +1,12 @@
 import com.boringguys.constantcontact.v2.ApiV2;
 import com.constantcontact.v2.account.AccountAddress;
 import com.constantcontact.v2.account.AccountSummaryInformation;
+import com.constantcontact.v2.contacts.ContactList;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 public class ApiMain
@@ -26,14 +28,52 @@ public class ApiMain
         getApiConfig();
         ApiV2 api = new ApiV2(apiKey, apiToken);
 
-        // get an API connection and pass it in
+        // always pass in an APIConn
         AccountSummaryInformation summary = api.getAccountSummary(api.getApiConn());
         printAccountSummaryInformation(summary);
 
+        List<ContactList> lists = api.getContactLists(api.getApiConn());
+        printContactLists(lists);
     }
 
     /**
-     * 
+     *
+     * @param lists
+     */
+    private void printContactLists(List<ContactList> lists)
+    {
+        if (lists == null || lists.size() == 0)
+        {
+            System.out.println("Can't find any Contact Lists -- check your API key and Token in properties file");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Found: " + lists.size() + " lists");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(System.getProperty("line.separator"));
+
+        for(ContactList list:lists)
+        {
+            sb.append("List name:    " + list.getName());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("List size:    " + list.getContactCount());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("List id:      " + list.getId());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("List created: " + list.getCreatedDate());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("List status:  " + list.getStatus());
+            sb.append(System.getProperty("line.separator"));
+            sb.append("--------------------------------");
+            sb.append(System.getProperty("line.separator"));
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    /**
+     *
      * @param summary
      */
     private void printAccountSummaryInformation(AccountSummaryInformation summary)
